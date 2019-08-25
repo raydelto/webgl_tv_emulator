00875264cd7f4d0871c3245227ebda074e839afb
Raydelto Hernandez <raydelto@gmail.com>
2019-08-25 09:27:29 -0300

    First version of no reception effect

--- js/webgl-demo.js
+++ js/webgl-demo.js
@@ -12,18 +12,34 @@ function main() {
   // Vertex shader program source
 
   const vsSource = `
-    attribute vec4 aVertexPosition;
+  attribute vec4 aVertexPosition;
+  varying vec4 vertexPosition;
 
-    void main() {
-      gl_Position =  aVertexPosition;
-    }
-  `;
+  void main()
+  {
+    vertexPosition =  aVertexPosition;
+    gl_Position =  aVertexPosition;
+  }
+`;
 
   // Fragment shader program source
 
   const fsSource = `
-    void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    precision mediump float;
+    varying vec4 vertexPosition;
+    float rand(vec2 co)
+    {
+        highp float a = 12.9898;
+        highp float b = 78.233;
+        highp float c = 43758.5453;
+        highp float dt= dot(co.xy ,vec2(a,b));
+        highp float sn= mod(dt,3.14);
+        return fract(sin(sn) * c);
+    }
+  
+    void main() 
+    {
+      gl_FragColor = vec4(rand(vertexPosition.xy),rand(vertexPosition.xy),rand(vertexPosition.xy),1);
     }
   `;
 
@@ -107,7 +123,7 @@ function loadShader(gl, type, source) {
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    alert('An error occurred compiling the '+ (type === gl.VERTEX_SHADER ? 'vertex' : 'fragment') +' shader:\n ' + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
     return null;
   }
