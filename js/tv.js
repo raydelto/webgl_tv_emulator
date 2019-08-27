745af34b702e5c49e66b839453ae0fa2230377d3
mecm1993 <mecm1993@gmail.com>
2019-08-27 09:31:42 -0400

    New channels btns added and minor change in controls view

--- js/tv.js
+++ js/tv.js
@@ -1,16 +1,25 @@
 const playButton = document.querySelector('.button');
-const upVolumeButton = document.querySelector('.up');
-const downVolumeButton = document.querySelector('.down');
+const upVolumeButton = document.querySelector('.volume > .up');
+const downVolumeButton = document.querySelector('.volume > .down');
+const upChannelButton = document.querySelector('.channel > .up');
+const downChannelButton = document.querySelector('.channel > .down');
 const muteButton = document.querySelector('.mute');
-const volumeDisplay = document.querySelector('.volume');
+const volumeDisplayer = document.querySelector('.volume-displayer');
+const channelDisplayer = document.querySelector('.channel-displayer');
+
 const tv = new TVRenderer();
 const noise = new Noise();
-let volume = 0;
+
+const MIN_CHANNEL = 1, MAX_CHANNEL = 99;
+let volume = 0, channel = '';
+
 tv.Off();
 
 function toggleButtonsDisabled(active) {
-  upVolumeButton.setAttribute('disabled', active);;
+  upVolumeButton.setAttribute('disabled', active);
   downVolumeButton.setAttribute('disabled', active);
+  upChannelButton.setAttribute('disabled', active);
+  downChannelButton.setAttribute('disabled', active);
   muteButton.setAttribute('disabled', active);
 }
 
@@ -20,8 +29,19 @@ function toggleMute(active) {
   muteButton.classList.add(active ? 'mute' : 'unmute');
 }
 
+function changeChannel(channel) {
+  // TODO TV should blink
+  channelDisplayer.innerHTML = channel;
+  // Hide channel number after 15 seconds
+  if (channel !== '') {
+    setTimeout(function() {
+      channelDisplayer.innerHTML = '';
+    }, 15000);
+  }
+}
+
 function adjustVolumePercentage(volume) {
-  volumeDisplay.innerHTML = `${volume}%`;
+  volumeDisplayer.innerHTML = `${volume}%`;
 }
 
 playButton.addEventListener('click', (e) => {
@@ -29,6 +49,7 @@ playButton.addEventListener('click', (e) => {
   if (eventTarget.classList.contains('play')) {
     toggleButtonsDisabled(false);
     adjustVolumePercentage(volume = 100);
+    changeChannel(channel = (channel === '') ? '1' : channel);
     tv.On();
     eventTarget.classList.remove('play');
     eventTarget.classList.add('pause');
@@ -36,6 +57,7 @@ playButton.addEventListener('click', (e) => {
   } else {
     toggleButtonsDisabled(true);
     adjustVolumePercentage(volume = 0);
+    changeChannel('');
     toggleMute(true);
     tv.Off();
     eventTarget.classList.remove('pause');
@@ -57,7 +79,17 @@ muteButton.addEventListener('click', (e) => {
     }
     noise.mute(eventTarget.id === 'activated');
   }
-})
+});
+
+upVolumeButton.addEventListener('click', (e) => {
+  const eventTarget = e.target;
+  if (eventTarget.getAttribute('disabled') === 'false') {
+    toggleMute(true);
+    if (noise.increase()) {
+      adjustVolumePercentage(volume += 5);
+    }
+  }
+});
 
 downVolumeButton.addEventListener('click', (e) => {
   const eventTarget = e.target;
@@ -69,13 +101,21 @@ downVolumeButton.addEventListener('click', (e) => {
   }
 });
 
-upVolumeButton.addEventListener('click', (e) => {
+upChannelButton.addEventListener('click', (e) => {
   const eventTarget = e.target;
-  if(eventTarget.getAttribute('disabled') === 'false') {
-    toggleMute(true);
-    if (noise.increase()) {
-      adjustVolumePercentage(volume += 5);
-    }
+  if (eventTarget.getAttribute('disabled') === 'false') {
+    channel = ((parseInt(channel) < MAX_CHANNEL)
+      ? parseInt(channel) + 1 : MIN_CHANNEL).toString();
+    changeChannel(channel);
+  }
+});
+
+downChannelButton.addEventListener('click', (e) => {
+  const eventTarget = e.target;
+  if (eventTarget.getAttribute('disabled') === 'false') {
+    channel = ((parseInt(channel) > MIN_CHANNEL)
+      ? parseInt(channel) - 1 : MAX_CHANNEL).toString();
+    changeChannel(channel);
   }
 });
 
